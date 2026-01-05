a1a0bee3b331a382e29211ecdfdc4689beab83e0
Parikshit Bhagwat <parixit23@gmail.com>
2026-01-05 22:42:11 +0900

    terraform initial checkins

---
+++ tf/ec2.tf
@@ -0,0 +1,30 @@
+data "aws_ami" "ubuntu" {
+  most_recent = true
+
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-jammy-22.04-amd64-server-*"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+
+  owners = ["099720109477"] # Canonical
+}
+
+resource "aws_instance" "example" {
+  ami           = data.aws_ami.ubuntu.id
+  instance_type = "t2.micro"
+  
+root_block_device {
+  volume_size = 8
+  volume_type = "gp2"
+}
+
+  tags = {
+    Name = "terraform-oidc-demo"
+    ExpiresOn   = "2026-02-01"
+  }
+}
